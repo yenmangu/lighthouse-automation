806000494c3722fcc70587bd19b5b6c1f4db860a
Robert John Shelford <robert.shelford@googlemail.com>
2026-01-02 22:09:33 +0000

    update(models): add Comment model; add slug to Subject; make migrations and migrate

--- resources/models.py
+++ resources/models.py
@@ -1,6 +1,6 @@
 from django.db import models
-from cloudinary.models import CloudinaryField
 from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
 
 # Create your models here.
@@ -34,6 +34,11 @@ class Subject(models.Model):
     """Stores a single Subject entity"""
 
     name = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(
+        max_length=200,
+        unique=True,
+        blank=True,
+    )
 
     def __str__(self):
         return f"{self.name}"
@@ -63,3 +68,23 @@ class Resource(models.Model):
 
     def __str__(self):
         return f"{self.title}"
+
+
+class Comment(models.Model):
+    """
+    Stores a single comment entry related to :model:`auth.User` & :model:`resources.Resource`
+    """
+
+    resource = models.ForeignKey(
+        Resource, on_delete=models.CASCADE, related_name="comments"
+    )
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="commenter")
+    body = models.TextField()
+    approved = models.BooleanField(default=False)
+    created_on = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ["-created_on"]
+
+    def __str__(self):
+        return f"Comment {self.body} by {self.author}"
