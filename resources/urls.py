52436600362633d8207c9ef2be9acf84f335cb75
Robert John Shelford <robert.shelford@googlemail.com>
2026-01-02 22:07:01 +0000

    feat(subject_resource_list): add view and template for list of subjects for specified resource

--- resources/urls.py
+++ resources/urls.py
@@ -2,5 +2,19 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.ResourceList.as_view(), name="home"),
+    path(
+        "",
+        views.ResourceList.as_view(),
+        name="home",
+    ),
+    path(
+        "resources/<slug:slug>/",
+        views.ResourceDetail.as_view(),
+        name="resource_detail",
+    ),
+    path(
+        "subjects/<slug:slug>/",
+        views.SubjetResourceListView.as_view(),
+        name="subject_resource_list",
+    ),
 ]
--- resources/views.py
+++ resources/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views import generic
-from .models import Resource
+from .models import Resource, Subject
 
 # Create your views here.
 
@@ -10,14 +10,59 @@ class ResourceList(generic.ListView):
     Class based view to show an ordered list of resources
     """
 
-    queryset = Resource.objects.filter(status=1)
+    queryset = Resource.objects.filter(status="p")
     template_name = "resources/index.html"
     paginate_by = 6
 
     def get_context(self, **kwargs):
         context = super.get_context_data(**kwargs)
+        print(f"context: {context}")
         page_obj = context.get("page_obj")
 
         return context
 
-        # TODO: Add POST conditional branch for comment submission
+
+class ResourceDetail(generic.DetailView):
+
+    model = Resource
+    template_name = "resources/resource_detail.html"
+    context_object_name = "resource"
+
+    def get_queryset(self):
+        """
+        Displays an individual :model:`resources.Resource`
+        :param self: The class instance
+
+        **Context**
+            *All variables returned to the template*
+        ``resource``
+            An instance of :model:`resources.Resource`
+        """
+        resource = Resource.objects.filter(status="p")
+
+        # TODO: Add POST and comment logic
+        return resource
+
+
+class SubjetResourceListView(generic.ListView):
+
+    model = Resource
+    template_name = "resources/subject_resource_list.html"
+    context_object_name = "resources"
+
+    def get_queryset(self):
+        self.subject = get_object_or_404(Subject, slug=self.kwargs["slug"])
+
+        return (
+            Resource.objects.filter(
+                subjects=self.subject,
+                status="p",
+            )
+            .select_related("author")
+            .prefetch_related("subjects")
+        )
+
+    def get_context_data(self, **kwargs) -> dict[str, Any]:
+        context = super().get_context_data(**kwargs)
+        context["subject"] = self.subject
+        return context
