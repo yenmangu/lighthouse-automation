5f675d6db00bbb0592eb1c1f71935737950427ea
Robert John Shelford <robert.shelford@googlemail.com>
2026-01-08 18:05:34 +0000

    chore: update settings.py

--- study_stack/urls.py
+++ study_stack/urls.py
@@ -22,5 +22,6 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("summernote", include("django_summernote.urls")),
     path("accounts/", include("allauth.urls")),
+    path("tinymce/", include("tinymce.urls")),
     path("", include("resources.urls"), name="resources-urls"),
 ]
