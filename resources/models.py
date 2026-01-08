472a6cf0974868929a756cc7a1cb26b56997b348
Robert John Shelford <robert.shelford@googlemail.com>
2026-01-08 18:51:27 +0000

    feat(Resource): add ordering to model

--- resources/models.py
+++ resources/models.py
@@ -66,6 +66,9 @@ class Resource(models.Model):
     created_on = models.DateTimeField(auto_now_add=True)
     updated_on = models.DateTimeField(auto_now=True)
 
+    class Meta:
+        ordering = ["-created_on"]
+
     def __str__(self):
         return f"{self.title}"
 
