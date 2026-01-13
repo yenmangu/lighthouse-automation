91ee8711818f18edb037993511f4006641f95fd8
Robert John Shelford <robert.shelford@googlemail.com>
2026-01-13 02:51:51 +0000

    chore: fix import on test

--- resources/test_model.py
+++ resources/test_model.py
@@ -1,7 +1,7 @@
 from django.test import TestCase
 from django.contrib.auth.models import User
 
-from .models import Resource
+from .models import Resource, Subject
 
 
 class TestUniqueSlug(TestCase):
